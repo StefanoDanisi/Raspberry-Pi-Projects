123ddb7489352cc5d58378fc085245a70c688ffa
Stefano Danisi <stefano_danisi@live.com>
2017-05-29 10:25:59 -0400

    Fan_Temp_Controller.py

--- Personal Projects/Fan_Temp_Controller.py
+++ Personal Projects/Fan_Temp_Controller.py
@@ -1,13 +1,17 @@
 #Stefano Danisi
-#Turn Fan 'ON' and 'OFF'
-#depending on CPU temperatures
+#Turn Fan 'ON' and 'OFF' depending on CPU temperatures
 
+#
+#Make sure to change GPIO PIN NUMBER to match your desired GPIO pin number 
+#
 
 import RPi.GPIO as GPIO
 import time
 import subprocess
 
 relay = 11 #GPIO Board pin number
+#To turn relay on set variable to 'False'
+#To turn relay off set variable to 'True' 
 relayStatus = True #Relay 'OFF'
 
 GPIO.setmode(GPIO.BOARD)
@@ -18,26 +22,28 @@ programRunning = True #Keep program running
 
 try:
     while programRunning:
-        tempCmd = subprocess.check_output(['vcgencmd', 'measure_temp'])
+        tempCmd = subprocess.check_output(['vcgencmd', 'measure_temp']) #Gets computer temperature
         tempCmdConvert = str(tempCmd, 'utf-8') #Convert tempCmd to string format
         #Convert String to int
         tempNum = int(float(tempCmdConvert.replace("temp=", "").replace("'C\n","")))
-
-        if tempNum >= 48 and relayStatus == True:
+        
+        #If the temperature is equal to or greater than desired temperature (55 degrees) the fan will turn on
+        #Change temperature value to  when you want the fan to engage
+        if tempNum >= 55 and relayStatus == True:
             print ("\nTurning Fan ON!")
             GPIO.output(relay, False)
             relayStatus = False
             for i in range(30): #Allows user to Keyboard Interrupt without waiting the entire sleep cycle
                 time.sleep(1) #Sleep until next temp read.
-        elif tempNum >= 48 and relayStatus == False:
+        elif tempNum >= 55 and relayStatus == False:
             print("\nFan is already ON!")
             for i in range(30):
                 time.sleep(1)
         elif tempNum < 55 and relayStatus == True:
-            print("\nTemps haven't reached above 49'C")  
+            print("\nTemps haven't reached above 54'C")  
             for i in range(30):
                 time.sleep(1)
-        elif tempNum < 48 and relayStatus == False:
+        elif tempNum < 55 and relayStatus == False:
             print("\nTurning Fan OFF!")
             GPIO.output(relay, True)
             relayStatus = True
